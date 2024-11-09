2669d76c94670e26b522450b97eb5983cdc69eb6
thdtjsdn <thdtjsdn@gmail.com>
2024-11-09 23:21:00 +0900

    Create .main.js

---
+++ .main.js
@@ -0,0 +1,10 @@
+
+// config
+
+// Windows
+global.SERVER.PATH_ROOT = 'D:/Videos/';
+
+// Linux & Mac
+//global.SERVER.PATH_ROOT = '/Videos/';
+
+//----------------------------------------------------------------------------------------------------;
